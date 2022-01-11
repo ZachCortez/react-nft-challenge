21043fc92c260a504ea883ff02b13b1b9a5b90cc
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-01-10 16:53:51 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Punklist from './components/Punklist';
 
 function App() {
     const [punkListData, setPunkListData] = useState([])
@@ -10,9 +11,10 @@ function App() {
     useEffect(() => {
       const getMyNfts = async () => {
         const openseaData = await axios.get(
-          'https://testnets-api.opensea.io/assets?asset_contract_address=0xD7B6aB09eb1F8598313E61cbAE482982CdC2e2C7order_direction=asc'
+          'https://testnets-api.opensea.io/assets?asset_contract_address=0xD7B6aB09eb1F8598313E61cbAE482982CdC2e2C7&order_direction=asc'
           )
           console.log(openseaData.data.assets)
+          setPunkListData(openseaData.data.assets)
       }
 
     return getMyNfts()
@@ -22,10 +24,12 @@ function App() {
     <div className='app'>
       <Header />
       <CollectionCard
-      id={0}
-      name={'SB Shades'}
-      traits={[{'value':7}]}
-      image='https://nftlabs.mypinata.cloud/ipfs/bafkreidqv7fjr7w4ovjsc5v7muld5wxxakpk63ke4p7v6hvggti3ogqgoe'/>
+        id={0}
+        name={'SB Shades'}
+        traits={[{'value':7}]}
+        image='https://nftlabs.mypinata.cloud/ipfs/bafkreidqv7fjr7w4ovjsc5v7muld5wxxakpk63ke4p7v6hvggti3ogqgoe'
+      />
+      <Punklist punkListData={punkListData} />
     </div>
   );
 }
